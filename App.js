4d5475fd419532719b6da77fd7f49c5b1486d849
Nikolai-desig <rinzleramad79@gmail.com>
2023-08-11 18:35:36 +0300

    старт воркінг

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StatusBar } from "expo-status-bar";
+import {ImageBackground, StyleSheet, Text, View } from "react-native";
+// import bgImage from '/Photo BGbg_photo.png'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  image: {
+    flex: 1,
     justifyContent: 'center',
   },
+  textStyle: {
+    color: 'white',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
+  },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={require('./img/bg_photo2x.png')} resizeMode="cover" style={styles.image}>
+        <Text style={styles.textStyle}>Inside</Text>
+      </ImageBackground>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
